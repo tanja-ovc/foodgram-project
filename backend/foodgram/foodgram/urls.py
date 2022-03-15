c88e6fb0e5ba6062a23e111f567fc15a5149cde2
Татьяна Овчинникова <tanja-ovc@yandex.ru>
2022-03-15 14:17:09 +0300

    testing Docker containers locally

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import (SpectacularAPIView, SpectacularRedocView,
@@ -14,4 +16,4 @@ urlpatterns = [
     path('api/auth/', include('djoser.urls.authtoken')),
     path('api/', include('api.urls')),
     path('api/', include('djoser.urls')),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
