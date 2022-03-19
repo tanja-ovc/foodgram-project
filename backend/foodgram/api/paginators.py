fb4572f75faedee37804e9edb6d17e10b1419b58
Татьяна Овчинникова <tanja-ovc@yandex.ru>
2022-03-19 03:38:22 +0300

    another pagination idea

--- backend/foodgram/api/paginators.py
+++ backend/foodgram/api/paginators.py
@@ -2,12 +2,4 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class CustomPageNumberPagination(PageNumberPagination):
-
-    def paginate_queryset(self, queryset, request, view=None):
-        if request.query_params.get('is_in_shopping_cart') == 1:
-            page_size = 999
-        else:
-            page_size = self.get_page_size(request)
-        if not page_size:
-            return None
-        return super().paginate_queryset(queryset, request)
+    page_size_query_param = 'limit'
--- backend/foodgram/api/views/recipe_views.py
+++ backend/foodgram/api/views/recipe_views.py
@@ -3,7 +3,6 @@ from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, permissions, status, viewsets
 from rest_framework.decorators import action
-# from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 from recipes.models import IngredientForRecipe, Recipe
